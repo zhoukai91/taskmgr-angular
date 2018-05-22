bd0a6ebf5191b676335b30bb8b49f0f2691942a6
vicet_zk <zhoukai8887@qq.com>
2018-05-22 22:40:49 +0800

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,19 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
+import {MatButtonModule} from '@angular/material'
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MatButtonModule,
+    CoreModule,
+   
   ],
   providers: [],
   bootstrap: [AppComponent]
